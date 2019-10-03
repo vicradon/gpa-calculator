ea2f5f4897005c6655ba2de624c98aac0dee40d0
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2019-10-03 04:53:48 +0100

    Revert "fix #5 increment the semesters listing"

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   * Then recalculate the gpa and display it to a user
   * 
 */
-let courseID = 1;
+
 
 const $ = n => document.querySelector(n);
 const $$ = n => document.querySelectorAll(n);
@@ -28,8 +28,8 @@ function calculateGPA() {
 
   Object.entries(activeCourses).forEach(([key, value]) => {
     //if (!activeCourses.hasOwnProperty(key)) {
-    tnu += +value[0];
-    tgp += (gradeVal[value[1].toLowerCase()] * +value[0]);
+      tnu += +value[0];
+      tgp += (gradeVal[value[1].toLowerCase()] * +value[0]);
     //}
   });
   const gpa = (tgp / tnu).toFixed(2);
@@ -80,68 +80,18 @@ function removeCourse() {
     };
   });
 }
-// this function would be a template that we can use to any thing with each single semester item.
-function generateSemesterNode(){
-  let semesterSection = document.createElement('section');
-  semesterSection.classList.add('gpcont');
-  semesterSection.innerHTML = `
-    <div class="gpcont-header">
-      <h3 class="semester">Semester <span class="semester-num"> ${courseID}</span></h3>
-      <p class="remove-semester">
-        &cross;
-      </p>
-    </div>
-    <div class="gptable">
-      <form class="gpitem">
-        <p>
-          <input id="course" type="text" name="course" class="gpa-item-input" placeholder="Course Name">
-        </p>
-
-        <p>
-          <select name="grade-select" class="grade-select">
-            <option value="--">Select Grade</option>
-            <option value="a">A</option>
-            <option value="b">B</option>
-            <option value="c">C</option>
-            <option value="d">D</option>
-            <option value="f">F</option>
-          </select>
-        </p>
-        <p>
-          <input id="credits" type="number" name="credits" class="gpa-item-input" placeholder="Credits">
-        </p>
-
-      </form>
-    </div>
-
-    <div style="display: none" id="course-table" class="course-table">
-      <div class="course-table-items">
-        <p class="course-name"></p>
-        <p class="course-grade"></p>
-        <p class="course-credits"></p>
-      </div>
-      <div onclick="removeCourse()" class="remove-course">
-        &cross;
-      </div>
-    </div>
 
 
-    <div class="gpcont-footer">
-      <div class="semester-gp">
-        <p>Semester&nbsp;<span class="semester-num">1&nbsp;</span>GPA:&nbsp;</p><span class="sem-gpa">0.00</span>
-      </div>
-      <button class="add-course">Add&nbsp;Course</button>
-    </div>`;
-
-  return semesterSection;
-}
-// adds a new semester to the listing.
 function addSemester() {
-  // increment the course id.
-  courseID+=1;
-  let clonedNode = generateSemesterNode();
-  $('main').appendChild(clonedNode);
-  log("appended");
+  let table = document.createDocumentFragment();
+  (function tableCreator() {
+    let clonedTemplate = $('.gpcont').cloneNode(true);
+    clonedTemplate.querySelector('.gpitem').reset();
+    table.appendChild(clonedTemplate);
+  })();
+  let semesterTable = table;
+  $('main').appendChild(semesterTable);
+  log("appended")
 }
 
 $$('.add-semester').forEach(button => button.onclick = addSemester);
